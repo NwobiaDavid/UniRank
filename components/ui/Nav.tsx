2e041200bb02fabd13e913e5c8fea2b38cfdf473
NwobiaDavid <dnwobia@gmail.com>
2024-07-31 23:15:56 +0100

    feat: made the navbar in themobile view responsive

--- components/ui/Nav.tsx
+++ components/ui/Nav.tsx
@@ -9,20 +9,23 @@ const Nav = () => {
   const session = useSession();
 
   return (
-    <div className="w-full flex justify-center z-50 fixed top-0 max-h-fit items-center p-5 ">
-      <div className=' xl:w-[40%] bg-white z-[70]  p-2 border rounded-xl flex justify-between items-center  ' >
+    <div className="w-full flex justify-center z-50 fixed top-0 max-h-fit items-center py-5 px-2 md:p-5 ">
+      <div className=' xl:w-[40%] lg:w-[50%] w-full bg-white z-[70]  p-2 border rounded-xl flex justify-between items-center  ' >
+        
         <div className="w-1/3 flex   ">
           <div className=' rounded-lg min-w-12 min-h-12 text-center bg-blue-300 ' >
             logo
           </div>
         </div>
-        <div className=' w-1/3 flex justify-center items-center '>
 
-        <h1 className='text-xl text-black font-bold' >UniRank</h1>
+        <div className=' w-1/3 flex justify-center items-center '>
+          <h1 className=' text-lg md:text-xl text-black font-bold' >UniRank</h1>
         </div>
+
         <div className=' w-1/3 flex justify-end items-end ' >
           <GetStartedButton session={session} />
         </div>
+
       </div>
     </div>
   )
--- components/ui/btn/GetStartedButton.tsx
+++ components/ui/btn/GetStartedButton.tsx
@@ -2,10 +2,9 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import useFetchData from "@/lib/useFetchSessionData";
 import { useEffect, useState } from 'react';
-import { useSession } from "next-auth/react";
 import { Session } from "next-auth";  
 
 interface GetStartedButtonProps {
@@ -13,16 +12,15 @@ interface GetStartedButtonProps {
 }
 
 const GetStartedButton: React.FC<GetStartedButtonProps> = ({ session }) => {
-    // const plausible = usePlausible();
     const router = useRouter();
-    // const { data: session } = useSession();
     const [val, setVal] = useState<string | undefined>(undefined);
+    const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             const data = await useFetchData();
-            console.log("--data type- "+data);
-            if (typeof data == 'string') {
+            console.log("--data type- " + data);
+            if (typeof data === 'string') {
                 setVal(data);
             }
         };
@@ -30,19 +28,27 @@ const GetStartedButton: React.FC<GetStartedButtonProps> = ({ session }) => {
         fetchData();
     }, []);
 
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768); // You can adjust the threshold as needed
+        };
+
+        handleResize(); // Initial check
+        window.addEventListener('resize', handleResize);
+        
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     const handleGetStarted = async () => {
         try {
-            console.log("-----val "+val);
-            console.log("-----session "+JSON.stringify(session));
+            console.log("-----val " + val);
+            console.log("-----session " + JSON.stringify(session));
             if (val !== undefined && val !== null) {
-                // if (session) {
-                    if (val === "true" ) {
-                        router.push("/home");
-                    } else {
-                        router.push("/onboarding");
-                    }
-                // }
+                if (val === "true") {
+                    router.push("/home");
+                } else {
+                    router.push("/onboarding");
+                }
             } else {
                 await signIn();
             }
@@ -52,9 +58,12 @@ const GetStartedButton: React.FC<GetStartedButtonProps> = ({ session }) => {
     };
 
     return (
-        <button onClick={handleGetStarted} className="hover:bg-black hover:text-white active:scale-95  duration-200 hover:border-opacity-100 border-opacity-65 border rounded-lg border-slate-800 bg-white px-4 py-2 flex items-center">
+        <button 
+            onClick={handleGetStarted} 
+            className="hover:bg-black md:text-base text-sm hover:text-white active:scale-95 duration-200 hover:border-opacity-100 border-opacity-65 border rounded-lg border-slate-800 bg-white px-2 md:px-4 py-2 flex items-center"
+        >
             <span>
-               {session ?  "Take the Test" : "HOME"}
+                {session ? (isMobile ? "Let's Start" : "Take the Test") : "HOME"}
             </span>
         </button>
     );
